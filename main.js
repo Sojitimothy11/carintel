34c10f3a8d044efa5fcbedf9c7a7d390573ee4f0
Adesoji Adeyemo <timothysoji1@gmail.com>
2023-12-12 06:52:58 +0100

    updated

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 
 // Set the date we're counting down to
-var countDownDate = new Date("September 30, 2023 15:37:25").getTime();
+var countDownDate = new Date("December 26, 2023 01:00:00").getTime();
 
 // Update the count down every 1 second
 var x = setInterval(function() {
